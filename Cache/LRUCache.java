ad83a0c334cc4bbbf48c7d4593874a71aa7c53a1
jhkim <jinhankim988@gmail.com>
2023-01-19 19:47:17 +0900

    [Fetch] LRU 캐싱 구현

---
+++ Cache/LRUCache.java
@@ -0,0 +1,30 @@
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRUCache <T> {
+	private int capacity = 100;
+	private Map<String, T> map = Collections.synchronizedMap(new LinkedHashMap<String, T>());
+	
+	public boolean containsKey(String key) {
+		return map.containsKey(key);
+	}
+	
+	public int getCapacity() {
+		return capacity;
+	}
+	
+	public void add(String key, T item) {
+		map.put(key, item);
+		if (map.size() > capacity) {
+			map.remove(map.keySet().iterator().next());
+		}
+	}
+	
+	public T get(String key) {
+		if (map.containsKey(key)) {
+			map.put(key, map.get(key));
+		}
+		return map.get(key);
+	}
+}
